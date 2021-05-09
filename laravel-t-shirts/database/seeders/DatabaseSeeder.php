7b872e7852d444cf96faea4a0c10b19bca888daf
Valentin Galit <valentin7galit@gmail.com>
2021-05-09 22:53:10 +0300

    Add seeder to Blog page

--- laravel-t-shirts/database/seeders/DatabaseSeeder.php
+++ laravel-t-shirts/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(20)->create();
+        \App\Models\Image::factory(20)->create();
+        \App\Models\Category::factory(20)->create();
+        \App\Models\Tag::factory(20)->create();
+        \App\Models\Article::factory(20)->create();
+        \App\Models\ArticleTag::factory(20)->create();
+        \App\Models\Gallery::factory(20)->create();
+        \App\Models\Comment::factory(20)->create();
     }
 }
