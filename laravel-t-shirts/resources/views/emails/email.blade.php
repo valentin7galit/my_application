a93781d3f0c138447366e2a51a09f16a580b9198
Valentin Galit <valentin7galit@gmail.com>
2021-05-16 02:11:00 +0300

    Modified a form page for email

--- laravel-t-shirts/resources/views/emails/email.blade.php
+++ laravel-t-shirts/resources/views/emails/email.blade.php
@@ -4,19 +4,11 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="@yield('description', 'Online shop for T-shirts system')">
         <meta name="author" content="Valentin Galit">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <!-- CSS -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-        <!-- Script -->
-        @yield('headerScripts')
         <!-- Font Icons -->
         <script src="https://kit.fontawesome.com/547107f345.js" crossorigin="anonymous"></script>
-        <!-- Title & Icon -->
-        <title>@yield('title', 'Online shop for T-shirts')</title>
-        <link rel="icon" href="./img/logo.png">
     </head>
     <body>
         <div class="bg-secondary text-center py-3">
