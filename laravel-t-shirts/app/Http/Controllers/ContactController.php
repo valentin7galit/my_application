53f56cf48f4ef2f8a380b28f1d2a2474b1170754
valentin7galit <68608972+valentin7galit@users.noreply.github.com>
2021-05-16 14:22:32 +0300

    Revert "Page Contact"

--- laravel-t-shirts/app/Http/Controllers/ContactController.php
+++ laravel-t-shirts/app/Http/Controllers/ContactController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Mail\Message;
-use Illuminate\Contracts\Mail\Mailer;
 
 class ContactController extends Controller
 {
@@ -13,25 +11,8 @@ class ContactController extends Controller
         return view('pages.contactpage');
     }
 
-    private Mailer $mailer;
-
-    public function __construct(Mailer $mailer)
+    public function contact(Request $request)
     {
-        $this->mailer = $mailer;
-    }
-
-    public function __invoke(Request $request)
-    {
-        $this->mailer->send(
-            'emails.email',
-            ['name' => $request->input('name'), 'content' => $request->input('content'), 'email' => $request->input('email')],
-            function (Message $message) use($request)
-            {
-                $message->to('valentin@t-shirts.com');
-                $message->subject($request->input('subject'));
-            }
-        );
-
-        return redirect('contact');
+        dd($request->input());
     }
 }
