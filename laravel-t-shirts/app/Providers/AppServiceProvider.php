cdf84e50437376c99ab440285f5989dc0c6cadb4
Valentin Galit <valentin7galit@gmail.com>
2021-05-25 22:54:28 +0300

    Add condition with application debug

--- laravel-t-shirts/app/Providers/AppServiceProvider.php
+++ laravel-t-shirts/app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use App\Services\DummyRequestLogger;
+use App\Services\BrowserRequestLogger;
+use App\Services\IPAddressRequestLogger;
 use App\Services\RequestLoggerInterface;
 use Illuminate\Support\ServiceProvider;
 use Psr\Log\LoggerInterface;
@@ -16,9 +17,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(RequestLoggerInterface:: class, DummyRequestLogger::class, function () {
-            return new DummyRequestLogger($this->app->make(LoggerInterface::class));
-        });
+        if (env('APP_DEBUG') === false) {
+            $this->app->bind(RequestLoggerInterface:: class, IPAddressRequestLogger::class, function () {
+                return new IPAddressRequestLogger($this->app->make(LoggerInterface::class));
+            });
+        } else {
+            $this->app->bind(RequestLoggerInterface:: class, BrowserRequestLogger::class, function () {
+                return new BrowserRequestLogger($this->app->make(LoggerInterface::class));
+            });
+        }
+        
     }
 
     /**
