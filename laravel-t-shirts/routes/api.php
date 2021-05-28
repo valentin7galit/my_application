9921a79bf9055b1009ed4948beb91b2d4f256282
Valentin Galit <valentin7galit@gmail.com>
2021-05-28 23:05:27 +0300

    Add Api routes

--- laravel-t-shirts/routes/api.php
+++ laravel-t-shirts/routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\ProductMenController;
+use App\Http\Controllers\ProductWomanController;
+use App\Http\Controllers\ProductKidController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductColourController;
+use App\Http\Controllers\ProductGalleryController;
+use App\Http\Controllers\ProductImageController;
+use App\Http\Controllers\ProductSizeController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +33,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/blog', [BlogController::class, 'indexApi']);
+Route::post('/blog', [BlogController::class, 'storeApi']);
+Route::get('/blog/article/{id}', [ArticleController::class, 'showApi']);
+Route::put('/blog/article/{id}', [ArticleController::class, 'updateApi']);
+Route::delete('/blog/article/{id}', [ArticleController::class, 'deleteApi']);
+Route::get('/blog/author/{id}', [UserController::class, 'showApi']);
+Route::get('/blog/category/{id}', [CategoryController::class, 'showApi']);
+Route::get('/blog/tag/{id}', [TagController::class, 'showApi']);
+Route::get('/blog/image/{id}', [ImageController::class, 'showApi']);
+Route::get('/blog/gallery/{id}', [GalleryController::class, 'showApi']);
+
+Route::get('/products/mens', [ProductMenController::class, 'indexApi']);
+Route::get('/products/women', [ProductWomanController::class, 'indexApi']);
+Route::get('/products/kids', [ProductKidController::class, 'indexApi']);
+Route::get('/products/{id}', [ProductController::class, 'showApi']);
+Route::put('/products/{id}', [ProductController::class, 'updateApi']);
+Route::delete('/products/{id}', [ProductController::class, 'deleteApi']);
+Route::get('/products/colour/all', [ProductColourController::class, 'indexApi']);
+Route::get('/products/colour/{id}', [ProductColourController::class, 'showApi']);
+Route::get('/products/gallery/all', [ProductGalleryController::class, 'indexApi']);
+Route::get('/products/gallery/{id}', [ProductGalleryController::class, 'showApi']);
+Route::get('/products/image/all', [ProductImageController::class, 'indexApi']);
+Route::get('/products/image/{id}', [ProductImageController::class, 'showApi']);
+Route::get('/products/size/all', [ProductSizeController::class, 'indexApi']);
+Route::get('/products/size/{id}', [ProductSizeController::class, 'showApi']);
