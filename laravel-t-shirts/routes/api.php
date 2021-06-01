7cc17a4e71165be0fea9df3379b173b8caa7faa9
Valentin Galit <valentin7galit@gmail.com>
2021-06-02 00:25:06 +0300

    Add Controllers and modified routes enpoints Api

--- laravel-t-shirts/routes/api.php
+++ laravel-t-shirts/routes/api.php
@@ -3,21 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\TagController;
-use App\Http\Controllers\ImageController;
-use App\Http\Controllers\GalleryController;
-use App\Http\Controllers\ProductMenController;
-use App\Http\Controllers\ProductWomanController;
-use App\Http\Controllers\ProductKidController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductColourController;
-use App\Http\Controllers\ProductGalleryController;
-use App\Http\Controllers\ProductImageController;
-use App\Http\Controllers\ProductSizeController;
+use App\Http\Controllers\Api\BlogsApiController;
+use App\Http\Controllers\Api\ArticlesApiController;
+use App\Http\Controllers\Api\AuthorsApiController;
+use App\Http\Controllers\Api\CategoriesApiController;
+use App\Http\Controllers\Api\TagsApiController;
+use App\Http\Controllers\Api\ImagesApiController;
+use App\Http\Controllers\Api\GalleriesApiController;
+use App\Http\Controllers\Api\ProductsMenApiController;
+use App\Http\Controllers\Api\ProductsWomanApiController;
+use App\Http\Controllers\Api\ProductsKidApiController;
+use App\Http\Controllers\Api\ProductsApiController;
+use App\Http\Controllers\Api\ProductsColourApiController;
+use App\Http\Controllers\Api\ProductsGalleryApiController;
+use App\Http\Controllers\Api\ProductsImagesApiController;
+use App\Http\Controllers\Api\ProductsSizeApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,28 +34,40 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/blog', [BlogController::class, 'indexApi']);
-Route::post('/blog', [BlogController::class, 'storeApi']);
-Route::get('/blog/article/{id}', [ArticleController::class, 'showApi']);
-Route::put('/blog/article/{id}', [ArticleController::class, 'updateApi']);
-Route::delete('/blog/article/{id}', [ArticleController::class, 'deleteApi']);
-Route::get('/blog/author/{id}', [UserController::class, 'showApi']);
-Route::get('/blog/category/{id}', [CategoryController::class, 'showApi']);
-Route::get('/blog/tag/{id}', [TagController::class, 'showApi']);
-Route::get('/blog/image/{id}', [ImageController::class, 'showApi']);
-Route::get('/blog/gallery/{id}', [GalleryController::class, 'showApi']);
+Route::get('/', function () {
+    return view('api.endpoints');
+});
 
-Route::get('/products/mens', [ProductMenController::class, 'indexApi']);
-Route::get('/products/women', [ProductWomanController::class, 'indexApi']);
-Route::get('/products/kids', [ProductKidController::class, 'indexApi']);
-Route::get('/products/{id}', [ProductController::class, 'showApi']);
-Route::put('/products/{id}', [ProductController::class, 'updateApi']);
-Route::delete('/products/{id}', [ProductController::class, 'deleteApi']);
-Route::get('/products/colour/all', [ProductColourController::class, 'indexApi']);
-Route::get('/products/colour/{id}', [ProductColourController::class, 'showApi']);
-Route::get('/products/gallery/all', [ProductGalleryController::class, 'indexApi']);
-Route::get('/products/gallery/{id}', [ProductGalleryController::class, 'showApi']);
-Route::get('/products/image/all', [ProductImageController::class, 'indexApi']);
-Route::get('/products/image/{id}', [ProductImageController::class, 'showApi']);
-Route::get('/products/size/all', [ProductSizeController::class, 'indexApi']);
-Route::get('/products/size/{id}', [ProductSizeController::class, 'showApi']);
+Route::get('/blog', [BlogsApiController::class, 'index']);
+Route::post('/blog', [BlogsApiController::class, 'store']);
+Route::get('/blog/articles/{id}', [ArticlesApiController::class, 'show']);
+Route::put('/blog/articles/{id}', [ArticlesApiController::class, 'update']);
+Route::delete('/blog/articles/{id}', [ArticlesApiController::class, 'delete']);
+Route::get('/blog/authors/{id}', [AuthorsApiController::class, 'show']);
+Route::delete('/blog/authors/{id}', [AuthorsApiController::class, 'delete']);
+Route::get('/blog/categories/{id}', [CategoriesApiController::class, 'show']);
+Route::delete('/blog/categories/{id}', [CategoriesApiController::class, 'delete']);
+Route::get('/blog/tags/{id}', [TagsApiController::class, 'show']);
+Route::delete('/blog/tags/{id}', [TagsApiController::class, 'delete']);
+Route::get('/blog/images/{id}', [ImagesApiController::class, 'show']);
+Route::delete('/blog/images/{id}', [ImagesApiController::class, 'delete']);
+Route::get('/blog/galleries/{id}', [GalleriesApiController::class, 'show']);
+Route::delete('/blog/galleries/{id}', [GalleriesApiController::class, 'delete']);
+
+Route::get('/products/mens', [ProductsMenApiController::class, 'index']);
+Route::post('/products/mens', [ProductsMenApiController::class, 'store']);
+Route::get('/products/women', [ProductsWomanApiController::class, 'index']);
+Route::post('/products/women', [ProductsWomanApiController::class, 'store']);
+Route::get('/products/kids', [ProductsKidApiController::class, 'index']);
+Route::post('/products/kids', [ProductsKidApiController::class, 'store']);
+Route::get('/products/{id}', [ProductsApiController::class, 'show']);
+Route::put('/products/{id}', [ProductsApiController::class, 'update']);
+Route::delete('/products/{id}', [ProductsApiController::class, 'delete']);
+Route::get('/products/colours/all', [ProductsColourApiController::class, 'index']);
+Route::get('/products/colours/{id}', [ProductsColourApiController::class, 'show']);
+Route::get('/products/galleries/all', [ProductsGalleryApiController::class, 'index']);
+Route::get('/products/galleries/{id}', [ProductsGalleryApiController::class, 'show']);
+Route::get('/products/images/all', [ProductsImagesApiController::class, 'index']);
+Route::get('/products/images/{id}', [ProductsImagesApiController::class, 'show']);
+Route::get('/products/sizes/all', [ProductsSizeApiController::class, 'index']);
+Route::get('/products/sizes/{id}', [ProductsSizeApiController::class, 'show']);
