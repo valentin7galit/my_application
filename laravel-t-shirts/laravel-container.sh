13a7697732ac1e28aaeaa0ca109f3dc41eb20be0
Valentin Galit <valentin7galit@gmail.com>
2021-06-02 00:30:38 +0300

    Deleted docker containers

--- laravel-t-shirts/laravel-container.sh
+++
@@ -1,7 +0,0 @@
-docker run --name laravel-t-shirts -d \
-    -p 88:80 \
-    -p 8822:22 \
-    --restart unless-stopped \
-    --volume /home/valentin/Internship_Development-Aid/My_Application/application/laravel-t-shirts:/var/www/html \
-    --network=laravel_t_shirts_network \
-    amoraresco/it-academy-project:latest \
--- laravel-t-shirts/mariadb-container.sh
+++
@@ -1,7 +0,0 @@
-docker run --name laravel-t-shirts-db -d \
-            -p 33066:3306 \
-            -e MYSQL_ROOT_PASSWORD=root \
-            --restart unless-stopped \
-            --volume /home/valentin/Internship_Development-Aid/My_Application/application/laravel-t-shirts/docker-containers/db:/var/lib/mysql \
-            --network=laravel_t_shirts_network \
-            mariadb:latest \
