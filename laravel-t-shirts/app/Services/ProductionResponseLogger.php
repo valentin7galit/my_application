eba55cb2da3b2ad0a79c34956455fba1769e161a
Valentin Galit <valentin7galit@gmail.com>
2021-06-03 14:34:54 +0300

    Created class with debug and production response

---
+++ laravel-t-shirts/app/Services/ProductionResponseLogger.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductionResponseLogger extends AbstractResponseLogger
+{
+    protected function extractResponsetData(Response $response): array
+    {
+        return [];
+    }
+
+    protected function handleFinishedRequestEvent(Response $response): void
+    {
+
+    }
+}
