57e54cb2ceb50e26686a4f7a66b90b3550b31083
Valentin Galit <valentin7galit@gmail.com>
2021-06-03 14:07:37 +0300

    Modified RequestLoger and add check inside of the bind, add ResponseLogger

--- laravel-t-shirts/app/Providers/AppServiceProvider.php
+++ laravel-t-shirts/app/Providers/AppServiceProvider.php
@@ -7,6 +7,10 @@ use App\Services\IPAddressRequestLogger;
 use App\Services\RequestLoggerInterface;
 use Illuminate\Support\ServiceProvider;
 use Psr\Log\LoggerInterface;
+use Illuminate\Support\ServicesProvider;
+use Illuminate\Events\Dispatcher;
+use App\Services\ResponseLoggerInterface;
+use App\Services\DebugResponseLogger;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,17 +20,18 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {
-        if (env('APP_DEBUG') === false) {
-            $this->app->bind(RequestLoggerInterface:: class, IPAddressRequestLogger::class, function () {
-                return new IPAddressRequestLogger($this->app->make(LoggerInterface::class));
-            });
-        } else {
-            $this->app->bind(RequestLoggerInterface:: class, BrowserRequestLogger::class, function () {
+    {   
+        $this->app->bind(RequestLoggerInterface::class, function() {
+            if ($this->app->environment('local')) {
                 return new BrowserRequestLogger($this->app->make(LoggerInterface::class));
-            });
-        }
-        
+            } else {
+                return new IPAddressRequestLogger($this->app->make(LoggerInterface::class));
+            }
+        });
+
+        $this->app->bind(ResponseLoggerInterface::class, function() {
+            return new DebugResponseLogger($this->app->make(LoggerInterface::class), $this->app->make(Dispatcher::class));
+        });
     }
 
     /**
