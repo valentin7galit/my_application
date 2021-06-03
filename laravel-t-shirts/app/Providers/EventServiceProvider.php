9cd54aba5eaee9059f84b9f466a6abd9b812740d
Valentin Galit <valentin7galit@gmail.com>
2021-06-03 14:10:16 +0300

    Add class EventServices on ResponseSent

--- laravel-t-shirts/app/Providers/EventServiceProvider.php
+++ laravel-t-shirts/app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        ResponseSent::class => [
+            ResponseProcessingListener::class,
+        ],
     ];
 
     /**
