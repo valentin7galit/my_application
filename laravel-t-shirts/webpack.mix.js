9016f2aa33e7b23e7894408c02ed8e7b133ac442
Valentin Galit <valentin7galit@gmail.com>
2021-06-16 02:22:17 +0300

    Add Product AJAX js file with Laravel Mix

--- laravel-t-shirts/webpack.mix.js
+++ laravel-t-shirts/webpack.mix.js
@@ -11,8 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/productsort.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js([
+    'resources/js/app.js', 
+    'resources/js/productsort.js',
+    'resources/js/productajax.js'
+], 'public/js');
+    
+mix.postCss('resources/css/app.css', 'public/css', [
+    //
+]);
