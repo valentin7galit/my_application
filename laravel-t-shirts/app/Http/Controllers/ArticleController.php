2f8aa19279b8571525c284dbbba44780225de0d0
Valentin Galit <valentin7galit@gmail.com>
2021-07-07 13:52:35 +0300

    Add comments in Artilce Controller

--- laravel-t-shirts/app/Http/Controllers/ArticleController.php
+++ laravel-t-shirts/app/Http/Controllers/ArticleController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Models\Comment;
 
 class ArticleController extends Controller
 {
     public function show($id)
     {
-        $articles = Article::with('images', 'categories', 'tags')
+        $articles = Article::with('images', 'categories', 'tags', 'galleries', 'author', 'comments')
             ->findOrFail($id);
         
+        $comments = Comment::with('authors', 'articles')
+            ->get();
+        
         return view('pages.article', [
-            'articles' => $articles
+            'articles' => $articles,
+            'comments' => $comments
         ]);
     }
 }
