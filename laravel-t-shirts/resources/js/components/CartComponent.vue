ffe084b2fa2ffb7992aee545d5f1f50baddd76ca
Valentin Galit <valentin7galit@gmail.com>
2021-07-15 14:31:03 +0300

    Modified template add tag span in CartComponent.vue

--- laravel-t-shirts/resources/js/components/CartComponent.vue
+++ laravel-t-shirts/resources/js/components/CartComponent.vue
@@ -1,9 +1,5 @@
 <template>
-    <a href="" class="button-cart btn btn-outline-secondary">
-        <i class="fas fa-shopping-cart"></i>
-        Cart
-        <span class="number-cart badge badge-pill badge-secondary">{{cartCount}}</span>
-    </a>
+    <span class="number-cart badge badge-pill badge-secondary">{{cartCount}}</span>
 </template>
 
 <script>
@@ -12,19 +8,7 @@ export default {
     computed: {
         cartCount() {
             return this.$root.cart.length;
-            /* const storageData = JSON.parse(localStorage.getItem('cart'));
-            return storageData.length */
         }
     }
 }
 </script>
-
-<style scoped>
-.button-cart {
-    margin-top: .10rem;
-}
-.button-cart:hover .number-cart {
-  background-color: #fff;
-  color: #6c757d;
-}
-</style>
