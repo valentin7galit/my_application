eeaae31e747afe2f5f71eaeb9fa583b1f28ee661
Valentin Galit <valentin7galit@gmail.com>
2021-07-15 18:34:50 +0300

    Add components for page Wishlist in app.js with Vue.js

--- laravel-t-shirts/resources/js/app.js
+++ laravel-t-shirts/resources/js/app.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
+
 import AddCartComponent from "./components/AddCartComponent.vue";
 import CartComponent from "./components/CartComponent";
 import ShowCartComponent from "./components/ShowCartComponent";
+
+import AddWishlistComponent from "./components/AddWishlistComponent";
+import WishlistComponent from "./components/WishlistComponent";
+import ShowWishlistComponent from "./components/ShowWishlistComponent";
 
 require('./bootstrap');
 
@@ -9,12 +14,17 @@ Vue.component('add-cart', AddCartComponent);
 Vue.component('cart-count', CartComponent);
 Vue.component('show-cart', ShowCartComponent);
 
+Vue.component('add-wishlist', AddWishlistComponent);
+Vue.component('wishlist-count', WishlistComponent);
+Vue.component('show-wishlist', ShowWishlistComponent);
+
 const app = new Vue({
     el: '#app',
     data() {
         return {
             /* Homework page refresh saving count cart */
-            cart: JSON.parse(localStorage.getItem('cart')) || []
+            cart: JSON.parse(localStorage.getItem('cart')) || [],
+            wishlist: JSON.parse(localStorage.getItem('wishlist')) || []
         }
     },
     methods: {
@@ -33,6 +43,22 @@ const app = new Vue({
 
             /* Homework add one product unique */
             const productIndex = this.cart.findIndex(product => product.id === productId.id);
+        
+            return productIndex !== -1;
+        },
+        addToWishlist(productId) {
+            if (!this.isProductAddedToWishlist(productId)) {
+                this.wishlist.push(productId);
+
+                localStorage.setItem('wishlist', JSON.stringify(this.wishlist));
+            }
+        },
+        isProductAddedToWishlist(productId) {
+            if (this.wishlist.length === 0) {
+                return false;
+            }
+
+            const productIndex = this.wishlist.findIndex(product => product.id === productId.id);
         
             return productIndex !== -1;
         }
