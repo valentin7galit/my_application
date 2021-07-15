6690e25b315bcf47f629cc00cb29e9558d479156
Valentin Galit <valentin7galit@gmail.com>
2021-07-15 14:13:52 +0300

    Add function sizes and lot_size in ProductController

--- laravel-t-shirts/app/Http/Controllers/ProductController.php
+++ laravel-t-shirts/app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ class ProductController extends Controller
 {
     public function show($id)
     {
-        $products = Product::with('product_images', 'colours', 'product_galleries')
+        $products = Product::with('product_images', 'colours', 'product_galleries', 'sizes', 'lot_sizes')
             ->findOrFail($id);
         
         return view('pages.product', [
